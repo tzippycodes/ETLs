5e0bf45925118b2dc17f3ed2952807617f1e722a
Tzippyisabird <tzippy@chesapeakeclimate.org>
2024-08-27 01:06:22 +0100

    Revised code to properly authenticate with the Engaging Networks API and save the file to a dataframe and locally. Also created some variables to enhance reusability for other APIs

--- EN_pg_ETL.py
+++ EN_pg_ETL.py
@@ -4,70 +4,8 @@
 import requests
 import pandas as pd
 import numpy as np
-import sqlite3
 from datetime import datetime 
-from sqlalchemy import create_engine
-# Connect to Engaging Networks Bulk API service with HTTPS GET Request to download user data and transactional data
-
-def authenticate(base_url, user_token):
-    return requests.post(base_url + "authenticate", data=user_token).json()["ens-auth-token"]
-
-def begin_export():
-
-    payload = {
-        "displayUserDataInTransactionExport": True,
-        "applyCustomReferenceNames": False,
-        "fileType": "csv",
-        "queryName": "EOY_Campaigns_ALL",
-        "format": "User data",
-        "fieldGroup": "apiexport"
-    }
-    headers = {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-        "ens-auth-token": "123"
-    }
-
-    response = requests.post(url, json=payload, headers=headers)
-
-    print(response.json())
-
-def check_progress():
-    pass
-def buffering_export():
-    pass
-def extract(url, response):
-    pass
-
-
-def load_to_csv(df, csv_path):
-    ''' This function saves the final dataframe as a `CSV` file 
-    in the provided path. Function returns nothing.'''
-    df.to_csv(csv_path)
-
-def transform(df):
-    ''' This function converts the GDP information from Currency
-    format to float value, transforms the information of GDP from
-    USD (Millions) to USD (Billions) rounding to 2 decimal places.
-    The function returns the transformed dataframe.'''
-    GDP_list = df["GDP_USD_millions"].tolist()
-    GDP_list = [float("".join(x.split(','))) for x in GDP_list]
-    GDP_list = [np.round(x/1000,2) for x in GDP_list]
-    df["GDP_USD_millions"] = GDP_list
-    df=df.rename(columns = {"GDP_USD_millions":"GDP_USD_billions"})
-    return df
-
-def load_to_db(df, sql_connection, table_name):
-    ''' This function saves the final dataframe to as a database table
-    with the provided name. Function returns nothing.'''
-    df.to_sql(table_name, sql_connection, if_exists='replace', index=False)
-
-def run_query(query_statement, sql_connection):
-    ''' This function runs the stated query on the database table and
-    prints the output on the terminal. Function returns nothing. '''
-    print(query_statement)
-    query_output = pd.read_sql(query_statement, sql_connection)
-    print(query_output)
+import time
 
 def timestamp():
     timestamp_format = '%Y-%h-%d-%H:%M:%S' # Year-Monthname-Day-Hour-Minute-Second 
@@ -81,25 +19,122 @@ def log_progress(message):
     with open("./etl_project_log.txt","a") as f: 
         f.write(timestamp() + ' : ' + message + '\n')
 
+# Connect to Engaging Networks Bulk API service with HTTPS GET Request to download user data and transactional data
+
+def authenticate(base_url, user_token):
+    return requests.post(base_url + "authenticate", data=user_token).json()["ens-auth-token"]
+
+def begin_export(base_url, ens_auth_token):
+
+    payload = {
+        "displayUserDataInTransactionExport": True,
+        "applyCustomReferenceNames": False,
+        "fileType": "csv",
+        "queryName": "Data_Administration_Bulk_Export_All_Users",
+        "format": "User data",
+        "fieldGroup": "apiexport"
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "ens-auth-token": ens_auth_token
+    }
+
+    response = requests.post(base_url + "exportjob", json=payload, headers=headers)
+
+    return response.json()['id']
+
+def check_progress(base_url, ens_auth_token, job_id):
+
+    headers = {
+        'Accept': "application/json",
+        'ens-auth-token': ens_auth_token
+    }
+
+    response = requests.get (base_url + "exportjob/" + str(job_id), headers=headers)
+
+    return response.json()['status']
+
+def waiting_for_export(base_url, ens_auth_token, job_id):
+
+    while check_progress(base_url, ens_auth_token, job_id) != "completed":
+        time.sleep(10)
+        log_progress("Checking export status.")
+    
+def extract(base_url, ens_auth_token, job_id):
+
+    headers = {
+        'Accept': "text/csv",
+        'ens-auth-token': ens_auth_token
+    }
+
+    response = requests.get (base_url + "exportjob/" + str(job_id) + "/download", headers=headers)
+
+    export_file_name = "export_"+ str(job_id) + ".csv"
+
+    export_file_csv = open(export_file_name, "w")
+
+    export_file_csv.write(response.text)
+
+    export_file_csv.close()
+
+    df = pd.read_csv(export_file_name)
+
+    return df
+
+def load_to_csv(df, csv_path):
+    ''' This function saves the final dataframe as a `CSV` file 
+    in the provided path. Function returns nothing.'''
+    df.to_csv(csv_path)
+
+def transform(df):
+
+
+def load_to_db(df, sql_connection, table_name):
+    ''' This function saves the final dataframe to as a database table
+    with the provided name. Function returns nothing.'''
+    df.to_sql(table_name, sql_connection, if_exists='replace', index=False)
+
+def run_query(query_statement, sql_connection):
+    ''' This function runs the stated query on the database table and
+    prints the output on the terminal. Function returns nothing. '''
+    print(query_statement)
+    query_output = pd.read_sql(query_statement, sql_connection)
+    print(query_output)
+
+
+
     
 ''' Here, you define the required entities and call the relevant 
 functions in the correct order to complete the project. Note that this
 portion is not inside any function.'''
+
 base_url = "https://us.engagingnetworks.app/ens/service/"
 user_token = open("user_token.txt", "r").readline().strip()
-table_attribs = [headers]
+#table_attribs = [headers]
 db_name = 'ccan.db'
 table_name = ''
-csv_path = "engaging_networks_backup_" + timestamp + ".csv"
+csv_path = "engaging_networks_backup_" + timestamp() + ".csv"
 
 log_progress('Preliminaries complete. Initiating ETL process')
 
-print(authenticate(base_url, user_token))
+ens_auth_token = authenticate(base_url, user_token)
+job_id = begin_export(base_url, ens_auth_token)
+print(job_id)
 
-df = begin_export()
+check_progress(base_url, ens_auth_token, job_id)
+
+waiting_for_export(base_url, ens_auth_token, job_id)
+
+log_progress('Data export complete. Initiating extraction process')
+
+df = extract(base_url, ens_auth_token, job_id)
+
+log_progress("File exported from Engaging Networks successfully.")
+
+print(df.head)
 
 '''
-log_progress('Data export complete. Initiating extraction process')
 df = extract_from_json(response)
 log_progress('Data extracted from JSON to Python DataFrame')
 df = transform(df)
